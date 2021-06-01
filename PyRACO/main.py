d5d812ff637305851f91357f356ae2d5bec4b681
Thiago <thiagoep@gmail.com>
2021-06-01 16:27:47 -0300

    maxflow

--- PyRACO/main.py
+++ PyRACO/main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import networkx as nx
+
 import lerInst as li
 import numpy as np
 import struct_graph as strgr
@@ -10,19 +12,24 @@ import networkx as nkx
 if __name__ == '__main__':
 
     n,adj,req,g = li.lerTXT('brasil.txt')
+    capacidade = 1
+    nx.set_edge_attributes(g, capacidade, "capacity")
+    print(g.edges(data=True))
+
     #print(g.number_of_nodes())
     #print(g.nodes())
     #print(g.edges())
     teste = np.array(req)
     listaReq = []
-    listaReq = ll.Link()
+#    listaReq = ll.Link()
     for i in range(g.number_of_nodes()):
         for j in range(g.number_of_nodes()):
             if teste[i][j]:
-                #cm = nks.maximum_flow(g,i,j)
-                cm=nkx.shortest_path_length(g,i,j) #Funcao para encontrar o valor do caminho minimo
+                mxf,cm = nkx.maximum_flow(g,i,j)
+                #cm=nkx.shortest_path_length(g,i,j) #Funcao para encontrar o valor do caminho minimo
                 for k in range(teste[i][j]):
-                    listaReq.append(strgr.Requisicao(i,j,cm))
+                    listaReq.append(strgr.Requisicao(i,j,mxf))
             else:
                 pass
 print(listaReq[0].i, listaReq[0].j, listaReq[0].qtd)
+
