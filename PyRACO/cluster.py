16574655cba422e9dcb809c010ab484bf367d773
ArturAlva <41214789+ArturAlva@users.noreply.github.com>
2021-12-02 21:12:34 -0300

    0212

--- PyRACO/cluster.py
+++ PyRACO/cluster.py
@@ -10,8 +10,11 @@ from pyclustering.utils.metric import type_metric, distance_metric
 
 
 def my_func(L1, L2):
-    C1 = nkx.shortest_path(grafo,L1[0],L1[1])
-    C2 = nkx.shortest_path(grafo,L2[0],L2[1])
+    inst = 'C:\\Users\\Artur Alvarenga\\Documents\\GitHub\\RACO\\Instâncias\\pickle\\eon.pickle'
+    I = strgr.lePickle(inst)
+    g = I.CriaGrafo()
+    C1 = nkx.shortest_path(g,L1[0],L1[1])
+    C2 = nkx.shortest_path(g,L2[0],L2[1])
     Camin1 = [(C1[m],C1[(m+1)]) for m in range(len(C1)-1)]
     Camin2 = [(C2[m],C2[(m+1)]) for m in range(len(C2)-1)]
     cinter = [i for i in Camin1 if i in Camin2]
@@ -28,10 +31,12 @@ if __name__ == '__main__':
     for x in range(len(Lreq)):
         Lij.append([Lreq[x].i,Lreq[x].j])
     metric = distance_metric(type_metric.USER_DEFINED, func=my_func)
-    distance = metric(Lij[10], Lij[12])
-    print(distance)
+    #distance = metric(Lij[10], Lij[12])
+    #print(distance)
     array = np.array(Lreq)
-    start_centers = [[0,0],[1,1],[2,2],[3,3]]
+    p = [2,1]
+    print(p[0])
+    start_centers = [p]
     kmeans_instance = kmeans(Lij, start_centers,metric=metric)
     kmeans_instance.process()
     clusters = kmeans_instance.get_clusters()
